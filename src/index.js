4de41283d35062233aeec643405e404bda56b20e
Enamul Hasan <mhasan173013@bscse.uiu.ac.bd>
2025-03-22 13:03:34 +0600

    utils error file update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ import mongoose from 'mongoose'
 import {DB_NAME} from './constants.js'
 import connectDB from './db/dbConnection.js'
 import dotenv from "dotenv"
+import express from 'express';
+
+const app = express()
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log('mongodb connection failed!!!',error)
+    process.exit(1)
+})
 
 
 
